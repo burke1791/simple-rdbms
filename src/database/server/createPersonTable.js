1e5e4fdd287efaa2a81cfabac95624e868ad1c86
Chris Burke <chris@hyperproof.io>
2022-09-22 12:12:38 -0400

    removed sqlite-parser

--- src/database/server/createPersonTable.js
+++ src/database/server/createPersonTable.js
@@ -1,5 +1,12 @@
 import { executeQuery } from '../queryProcessor';
-import sqliteParser from 'sqlite-parser';
+import { Parser } from 'node-sql-parser';
+import { extractSingleQueryTree } from '../queryProcessor/treeParser';
+
+const parser = new Parser();
+
+const parserConfig = {
+  database: 'TransactSQL'
+};
 
 /**
  * @function
@@ -8,27 +15,20 @@ import sqliteParser from 'sqlite-parser';
  */
 export function createPersonTable(buffer) {
   const createQuery = `
-    Create Table dbo.person (
-      person_id Int Primary Key Autoincrement Not Null,
-      first_name Varchar(100) Not Null,
-      middle_name Varchar(100) Null,
-      last_name Varchar(100) Not Null,
-      age Int Not Null,
-      height_inches SmallInt Null,
-      weight_lbs Int Null
+    Create Table dbo.Person (
+      PersonId Int Not Null Identity(1, 1) Primary Key,
+      FirstName Varchar(100) Not Null,
+      MiddleName Varchar(100) Null,
+      LastName Varchar(100) Not Null,
+      Age Int Not Null,
+      HeightInches SmallInt Null,
+      Weight Int Null
     )
   `;
 
-  const tree = sqliteParser(createQuery);
-  let queryTree;
+  const tree = parser.astify(createQuery, parserConfig);
 
-  if (tree.type == 'statement' && tree.variant == 'list') {
-    if (tree.statement.length > 1) {
-      throw new Error('Only one query at a time is currently supported');
-    } else {
-      queryTree = tree.statement[0];
-    }
-  }
+  const queryTree = extractSingleQueryTree(tree);
 
   console.log(queryTree);
 
